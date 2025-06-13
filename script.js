8ca66d8281ec70e3650a266907d79a4d4ba4be29
Alexandre Silva <84294748+Alexandre-Paulo-Silva@users.noreply.github.com>
2025-06-13 11:59:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function addRecommendation() {
+  // Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+    console.log("New recommendation added");
+    //Call showPopup here
+
+    // Create a new 'recommendation' element and set it's value to the user's message
+    var element = document.createElement("div");
+    element.setAttribute("class","recommendation");
+    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    // Add this element to the end of the list of recommendations
+    document.getElementById("all_recommendations").appendChild(element); 
+    
+    // Reset the value of the textarea
+    recommendation.value = "";
+
+    showPopup(true); 
+  }
+}
+
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+}
+
+document.querySelectorAll('.recommendation').forEach(recommendation => {
+  let lines = recommendation.querySelectorAll('.line');
+  let delay = 0;
+
+  lines.forEach(line => {
+    setTimeout(() => {
+      line.style.animation = 'typing 2s steps(40) forwards, blink 0.7s infinite';
+    }, delay);
+    delay += 2000; // Define um tempo entre cada linha para aparecer uma por vez
+  });
+});
